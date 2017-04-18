217e40caa4a967499644458170bbd81d806ba243
Nate <nate@logicalengineer.com>
2017-04-17 21:07:47 -0500

    Init

---
+++ LogicalEngineer.Blog.Ping/PingTest.cs
@@ -0,0 +1,93 @@
+﻿/*
+   The MIT License (MIT)
+   Copyright (c) 2015 Logical Engineer
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, 
+   including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished 
+   to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+   THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace LogicalEngineer.Blog.Ping
+{
+    public class PingTest
+    {
+        private readonly System.Net.NetworkInformation.Ping _ping;
+
+        public PingTest()
+        {
+            this._ping = new System.Net.NetworkInformation.Ping();
+        }
+
+        public Task<PingReply> Ping(IPAddress ipAddress)
+        {
+            return this._ping.SendPingAsync(ipAddress);
+        }
+
+        /// <summary>
+        /// The response we sind from the FindIPAddress. It'll
+        /// contain the IPAddress and any error info.
+        /// </summary>
+        public class IPAddressResponse
+        {
+            public IPAddress IPAddress { get; set; }
+            public Boolean IsSuccessful { get { return this.IPAddress != null; } }
+            public String ErrorMessage { get; set; }
+        }
+
+
+        public async Task<IPAddressResponse> FindIPAddress(String hostName)
+        {
+            var response = new IPAddressResponse();
+
+            if (!String.IsNullOrWhiteSpace(hostName))
+            {
+                try
+                {
+                    //IPAddress.Parse method is technically faster than the DNS.GetHostAddresses, but can't resolve host names.
+                    //However, the speed improvement you get out of IPAddress.Parse will not be worth the extra code to maintain unless
+                    //you're calling this in a tight loop in a critical piece of code. If you do, you can simply write a if statement around
+                    //this section of code checking for an IP address and calling IPAddress.Parse if the user pased an IP Address instead of a hostname
+
+                    //The Dns.GetHostAddresses relies on a DNS server to provide it the information, so it can be somewhat slow compared to conventional parsing
+                    //of IP Addresses. However, it's needed to resolve the Hostnames.
+                    var addresslist = await Dns.GetHostAddressesAsync(hostName);
+
+                    //There can be multiple ip addresses (think of load balancing)
+                    if (addresslist.Length > 0)
+                        response.IPAddress = addresslist[0];
+                }
+                //The length of hostNameOrAddress is greater than 255 characters.
+                catch (ArgumentOutOfRangeException argumentRangeException)
+                {
+                    response.ErrorMessage = argumentRangeException.Message;
+                }
+                //An error is encountered when resolving the hostName.
+                catch (SocketException socketException)
+                {
+                    response.ErrorMessage = socketException.Message;
+                }
+                //hostName is an invalid address.
+                catch (ArgumentException argumentException)
+                {
+                    response.ErrorMessage = argumentException.ParamName;
+                }
+            }
+            else
+            {
+                response.ErrorMessage = "No Hostname or IP Address provided.";
+            }
+
+            return response;
+        }
+    }
+}
---
+++ LogicalEngineer.Blog.Ping/Program.cs
@@ -0,0 +1,120 @@
+﻿/*
+   The MIT License (MIT)
+   Copyright (c) 2015 Logical Engineer
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, 
+   including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished 
+   to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+   THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace LogicalEngineer.Blog.Ping
+{
+    class Program
+    {
+        private const Int32 Tries = 6;
+        private List<PingReply> Responses = new List<PingReply>();
+        private IPAddress IPAddress;
+        private Boolean HasPrintedStats = false;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            Console.CancelKeyPress += delegate
+            {
+                //When the user presses CTRL + C, we will print the statistics we have and then stop.
+                program.PrintStatistics();
+            };
+
+            if (args != null && args.Length == 1)
+                program.Run(args[0]);
+            else
+                Console.WriteLine("LogicalBit.Ping.exe ipOrHostName");
+        }
+
+        public void Run(String ipOrHostName)
+        {
+            var pingTest = new PingTest();
+
+            var response = pingTest.FindIPAddress(ipOrHostName).Result;
+
+            if (response.IsSuccessful)
+            {
+                this.IPAddress = response.IPAddress;
+                Console.WriteLine(String.Format("Pinging {0} [{1}] with 32 bytes of data:", ipOrHostName, response.IPAddress.ToString()));
+
+                for (var i = 0; i < Tries; i++)
+                {
+                    var pingResponse = pingTest.Ping(response.IPAddress).Result;
+                    this.Responses.Add(pingResponse);
+
+                    if (pingResponse.Status == IPStatus.Success)
+                    {
+                        Console.WriteLine(String.Format(
+                                  "Reply from {0}: bytes={1} time={2}ms TTL={3}"
+                                , pingResponse.Address.ToString()
+                                , pingResponse.Buffer.Length
+                                , pingResponse.RoundtripTime
+                                , pingResponse.Options.Ttl
+                        ));
+                    }
+                    else
+                    {
+                        Console.WriteLine(pingResponse.Status.ToString());
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                PrintStatistics();
+            }
+        }
+
+        /// <summary>
+        /// Aggregate and print high level detail about the PING test.
+        /// </summary>
+        public void PrintStatistics()
+        {
+            if (this.HasPrintedStats)
+                return;
+
+            var successPacketCount = this.Responses.Where(x => x.Status == IPStatus.Success).Count();
+            var failurePacketCount = this.Responses.Where(x => x.Status != IPStatus.Success).Count();
+
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Format("Ping statistics for {0}:", this.IPAddress.ToString()));
+
+            Console.WriteLine(String.Format(
+                 "\tPackets: Sent = {0}, Received = {1}, Lost = {2} ({3}% loss)"
+                , this.Responses.Count
+                , successPacketCount
+                , failurePacketCount
+                , failurePacketCount == 0 ? 0M : (((failurePacketCount - successPacketCount) / failurePacketCount) * 100M)
+            ));
+
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("Approximate round trip times in milli-seconds:");
+
+            Console.WriteLine(String.Format(
+                 "\tMinimum = {0}ms, Maximum = {1}ms, Average = {2}ms"
+                , this.Responses.Min(x => x.RoundtripTime)
+                , this.Responses.Max(x => x.RoundtripTime)
+                , this.Responses.Average(x => x.RoundtripTime).ToString("#0.00")
+            ));
+
+
+            this.HasPrintedStats = true;
+        }
+    }
+}
